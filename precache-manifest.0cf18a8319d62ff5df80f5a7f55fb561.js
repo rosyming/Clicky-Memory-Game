897448202bf10e61b0541cd217459267f6c96d59
Rosyming <41979276+rosyming@users.noreply.github.com>
2019-01-16 00:26:05 -0500

    Updates

---
+++ precache-manifest.0cf18a8319d62ff5df80f5a7f55fb561.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c6f50a71e508c17f116",
+    "url": "/Clicky-Memory-Game/static/js/runtime~main.6c6f50a7.js"
+  },
+  {
+    "revision": "c77f4a2bf6ba20aaba43",
+    "url": "/Clicky-Memory-Game/static/js/main.c77f4a2b.chunk.js"
+  },
+  {
+    "revision": "790a7416686c195da3c6",
+    "url": "/Clicky-Memory-Game/static/js/1.790a7416.chunk.js"
+  },
+  {
+    "revision": "c77f4a2bf6ba20aaba43",
+    "url": "/Clicky-Memory-Game/static/css/main.5a98e327.chunk.css"
+  },
+  {
+    "revision": "bf72ee10f07b543b13ac5724df663041",
+    "url": "/Clicky-Memory-Game/index.html"
+  }
+];
